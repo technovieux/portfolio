9e530e864a76201c0b8674bb183ba7dfc3b7fec6
mathiasC <105153480+technovieux@users.noreply.github.com>
2025-11-26 08:46:22 +0100

    ordonate subpages

---
+++ sub_pages/script.js
@@ -0,0 +1,34 @@
+const hamburger = document.getElementById('hamburger');
+const menu = document.querySelector('.menu');
+
+hamburger.addEventListener('click', function () {
+    const hamIcon = this.querySelector('.hamburger-icon');
+    const crossIcon = this.querySelector('.cross-icon');
+    if (hamIcon.style.display === "none") {
+        hamIcon.style.display = "inline-block"
+        menu.style.display = "none"
+        crossIcon.style.display = "none"
+    }
+    else {
+        crossIcon.style.display = "inline-block"
+        hamIcon.style.display = "none"
+        menu.style.display = "block"
+    }
+});
+
+function telechargerPDF() {
+    const pdfurlen = './images_files/CV_mathias_cassegrain_en.pdf';
+    const pdfurlfr = './images_files/CV_mathias_cassegrain_fr.pdf';
+    const link = document.createElement('a');
+    link.href = pdfurlen;
+    if (userLang.toLowerCase().startsWith('fr')) {
+        link.href = pdfurlfr;
+    }
+    link.download = 'CV_mathias_cassegrain.pdf'; // Optional: specify a default file name
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+
+}
+
+
