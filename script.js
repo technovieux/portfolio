c26a54b9204b738499bb46afbd2a9af3c3192648
mathiasC <105153480+technovieux@users.noreply.github.com>
2025-12-01 00:32:08 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const hamburger = document.getElementById('hamburger');
 const menu = document.querySelector('.menu');
+const menuLinks = document.querySelectorAll('.menu-list-items a');
 
 hamburger.addEventListener('click', function () {
     const hamIcon = this.querySelector('.hamburger-icon');
@@ -30,5 +31,19 @@ function telechargerPDF() {
     document.body.removeChild(link);
 
 }
+
+
+function fermerMenu() {
+    const hamIcon = hamburger.querySelector('.hamburger-icon');
+    const crossIcon = hamburger.querySelector('.cross-icon');
+
+    crossIcon.style.display = "none";
+    hamIcon.style.display = "inline-block";
+    menu.style.display = "none";
+}
+
+menuLinks.forEach(link => {
+    link.addEventListener('click', fermerMenu);
+});
 
 
