9dc9bddee2786e174bb70c6e61191e3c54c376bd
mathiasC <105153480+technovieux@users.noreply.github.com>
2025-12-01 00:32:39 +0100

    Update script.js

--- sub_pages/script.js
+++ sub_pages/script.js
@@ -1,5 +1,6 @@
 const hamburger = document.getElementById('hamburger');
 const menu = document.querySelector('.menu');
+const menuLinks = document.querySelectorAll('.menu-list-items a'); 
 
 hamburger.addEventListener('click', function () {
     const hamIcon = this.querySelector('.hamburger-icon');
@@ -14,4 +15,19 @@ hamburger.addEventListener('click', function () {
         hamIcon.style.display = "none"
         menu.style.display = "block"
     }
+
+});
+
+
+function fermerMenu() {
+    const hamIcon = hamburger.querySelector('.hamburger-icon');
+    const crossIcon = hamburger.querySelector('.cross-icon');
+
+    crossIcon.style.display = "none";
+    hamIcon.style.display = "inline-block";
+    menu.style.display = "none";
+}
+
+menuLinks.forEach(link => {
+    link.addEventListener('click', fermerMenu);
 });
